051aa4254da2b54297b6ddea878865d155bcb41a
Sergi <sergi20031005@gmail.com>
2023-11-02 19:24:48 +0100

    Add phone visuals

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,1 +1,15 @@
-export function App() {}
+import { Actions } from '../actions/actions';
+import { Info } from '../info/info';
+import { Keyboard } from '../keyboard/keyboard';
+
+export function App() {
+  return (
+    <>
+      <Info></Info>
+      <main className="phone">
+        <Keyboard></Keyboard>
+        <Actions></Actions>
+      </main>
+    </>
+  );
+}
